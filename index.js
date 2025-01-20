05d64abddb1894341ea6d21cf80a5c56884236e6
Brandon Bakus <brandonbakus@gmail.com>
2025-01-19 20:48:47 -0500

    Done

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-/* Your Code Here */
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+
+
+function createEmployeeRecord(employee){
+    
+    const employeeInfo = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeInfo
+
+}
+
+function createEmployeeRecords(employeeArrays) {
+    return employeeArrays.map(employeeArray => createEmployeeRecord(employeeArray));
+}
+
+
+function createTimeInEvent(dateStamp) {    
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: dateStamp.split(" ")[0],
+        hour: parseInt(dateStamp.split(" ")[1])
+    });
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: dateStamp.split(" ")[0],
+        hour: parseInt(dateStamp.split(" ")[1])
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(event => event.date === date);
+    let timeOut = this.timeOutEvents.find(event => event.date === date);
+    
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    let hours = hoursWorkedOnDate.call(this, date);
+    return hours * this.payPerHour;
+}
+
+
+function findEmployeeByFirstName(employees, searchName) {
+    return employees.find(employee => employee.firstName === searchName);
+}
+
+function calculatePayroll(payroll) {
+    return payroll.reduce((total, employee) => {
+      return total + allWagesFor.call(employee);
+    }, 0);
+  }
+
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
